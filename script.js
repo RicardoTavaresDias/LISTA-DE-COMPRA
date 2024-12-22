75f99f8edd8f16e5472ba5b2ca976e15b1a8d624
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2024-12-22 15:03:01 -0300

    itens sendo excluido

--- script.js
+++ script.js
@@ -1,62 +1,110 @@
-const form = document.querySelector("form")
-const item = document.getElementById("item")
-const checkbox = document.querySelectorAll('input[type="checkbox"]')
-
-
+let form = document.querySelector("form")
+let item = document.getElementById("item")
+let checkbox = document.querySelector('input[type="checkbox"]')
 
 
 form.addEventListener ("submit", (event) => {
   event.preventDefault()
 
   // Adicionando o Item na lista
-  const div = document.createElement("div")
-  const label = document.createElement("label")
-  const input = document.createElement("input")
-  const img = document.createElement("img")
-  const a = document.createElement("a")
-  const span = document.createElement("a")
+  let div = document.createElement("div")
+  let label = document.createElement("label")
+  let input = document.createElement("input")
+  let img = document.createElement("img")
+  let a = document.createElement("a")
 
 
   div.className = item.value
 
-  label.id = "itens"
+  // label.name = "itens"
+  // label.id = "itens"
   label.textContent = item.value
-  div.appendChild(label)
+  div.append(label)
 
   input.type = "checkbox"
   input.name = "itens"
-  input.id = "itens"
-  label.appendChild(input)
+  input.id = item.value
+  input.value = item.value
+  label.append(input)
   
   a.href = "#"
-  div.appendChild(a)
+  a.id = "excluir"
+  //a.style.cursor = "pointer"
+  a.setAttribute("onclick", `excluirProduto("${item.value}")`)
+  div.append(a)
 
   img.src = "assets/icon/exclusao.svg"
-  img.alt = ""
-  a.appendChild(img)
+  img.alt = " "
+  a.append(img)
+
+  
 
 document.querySelector(".container-lista").append(div)
 
-  console.log(div)
- 
-
-  // Removendo o item na lista
-  checkbox.forEach((element) => {
-
-    if (element.checked) {
-      // console.log("checked!")
-      // console.log(element.value)
-    
-      document.querySelector(".cafe").remove(div)
-
-  
-
-    
-    }else {
-      console.log("no checked!")
-    }
-
-  })
-
+  console.log(div)  
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function excluirProduto(id) {
+  const checkboxChecked = document.querySelector(`#${id}`).checked
+  let div = document.createElement("div")
+
+  if (checkboxChecked) {
+    console.log("checked!")
+    console.log(id)
+    document.querySelector(`.${id}`).remove(div)
+  } else {
+    console.log("no checked!")
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+// // Removendo o item na lista
+// removeItem.forEach((itemRemove) => {
+
+// itemRemove.addEventListener("click", (event) => {
+  
+//   checkbox.forEach((element) => {
+
+//     if (element.checked) {
+//       // console.log("checked!")
+//       console.log(element.value)
+  
+//       let div = document.createElement("div")
+//       //document.querySelector(`.${element.value}`).remove(div)
+      
+   
+//     }else {
+//       console.log("no checked!")
+//     }
+  
+//   })
+//   })
+
+// })
+
+
+
+
 
